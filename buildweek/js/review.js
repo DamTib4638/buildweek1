0867685b84ffc9f7a822f3ee0b1b7e697691cd9b
francescopasta <pastore.francesco01@gmail.com>
2022-11-14 16:44:24 +0000

    review francesco_16:44

---
+++ buildweek/js/review.js
@@ -0,0 +1,10 @@
+/*let svgs = document.querySelectorAll('svg')
+let array = []
+for (let el of svgs) {
+    array.push(el)
+    el.addEventListener('mouseover',(e) => {
+        if (e < array[2]) {
+            e.classList.add('path')
+        }
+    })
+}*/
