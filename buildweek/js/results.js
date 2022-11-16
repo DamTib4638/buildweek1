64060902d0b7b70f34375e912ced271b01fa687f
Gavvio <steve.gavvio@gmail.com>
2022-11-16 16:44:40 +0100

    grafico torta

---
+++ buildweek/js/results.js
@@ -0,0 +1,42 @@
+var giuste=8;
+    var sbagliate=2;
+  const data = {
+    labels: [
+      'Blue',
+      'Purple'
+    ],
+    datasets: [{
+      label: 'domande',
+      data: [sbagliate,giuste],
+      backgroundColor: [
+        '#D20094',
+        '#00FFFF'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  const config = {
+    type: 'doughnut',
+    data: data,
+  };
+  const ctx = document.getElementById('myChart');
+  
+  new Chart(ctx,{
+    type: 'doughnut',
+  data: data,
+  options: {
+    cutout:140,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        display:false
+      },
+      title: {
+        display: false,
+        text: 'Chart.js Doughnut Chart'
+      }
+    }
+  },
+
+  });
